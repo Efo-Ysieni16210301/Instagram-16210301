425c8b844c1ac806ea027b5e915ca988b45ebafe
Efo-Ysieni16210301 <aaron162103@gmail.com>
2026-09-01 21:59:27 +0300

    add the menu and profile image at the right side of the header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import React from "react";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import {
+  MagnifyingGlassIcon,
+  PlusCircleIcon,
+} from "@heroicons/react/24/outline";
+import { HomeIcon } from "@heroicons/react/16/solid";
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between max-w-6xl ">
+    <div className="flex items-center justify-between max-w-6xl mx-4  ">
       {/* Left */}
       <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
         <Image
@@ -38,7 +42,16 @@ export default function Header() {
       </div>
 
       {/* Right */}
-      <h1>Right sides</h1>
+      <div className="flex space-x-4 items-center">
+        <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+
+        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+        <img
+          src="https://media.licdn.com/dms/image/v2/D4E03AQGEX_vk_Qc6Qg/profile-displayphoto-crop_800_800/B4EZmkzGOjGcAI-/0/1759406484294?e=1789603200&v=beta&t=txjbCCMIpHP67XrZkEdNAI8v8k-Dj_gjpCeNpBLWTuk"
+          alt="user-image"
+          className="h-10 rounded-full cursor-pointer"
+        />
+      </div>
     </div>
   );
 }
