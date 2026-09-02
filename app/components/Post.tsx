95f6d5eabe7fd9d75601bc681d6523d44d1fa538
Efo-Ysieni16210301 <aaron162103@gmail.com>
2026-09-02 20:48:42 +0300

    create the header and image sections of the post component

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { EllipsisHorizontalIcon } from "@heroicons/react/24/outline";
 interface PostProps {
   id: string;
   username: string;
@@ -15,8 +16,19 @@ export default function Post({
   caption,
 }: PostProps) {
   return (
-    <div>
-      <h1>{username}</h1>
+    <div className="bg-white my-7 border border-gray-200 rounded-md">
+      {/* Post Header */}
+      <div className="flex items-center p-5">
+        <img
+          className="h-12 rounded-full object-cover border p-1 mr-3  "
+          src={userImg}
+          alt={username}
+        />
+        <p className="font-bold text-sm flex-1 ">{username}</p>
+        <EllipsisHorizontalIcon className="h-5" />
+      </div>
+      {/* Post Image */}
+      <img className="object-cover w-full" src={img} alt={caption} />
     </div>
   );
 }
